b152426a54d402a85095f759b1891de50a13b180
write2cmdg <write2cmdg@gmail.com>
2023-04-04 03:14:00 -0400

    installed axios connected front back end and db

--- src/pages/BossUpdatePage.jsx
+++ src/pages/BossUpdatePage.jsx
@@ -9,7 +9,7 @@ const BossUpdatePage = () => {
    
       <h2 className='text-xl md:text-2xl lg:text-3xl mx-auto w-2/3 p-6 text-3xl text-blue-900'>Update boss</h2>
         <div className="w-screen min-h-[75%] flex bg-blue-100/50">
-            {/* content */}
+          {/* content */}
         </div>
         <div className='z-50 text-xs md:text-lg lg:text-1xl mx-auto my-4 w-screen sm:w-2/3 flex flex-row justify-around'>
         </div> 
