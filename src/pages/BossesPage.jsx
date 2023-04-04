60d8173458b84352c0b404a4a8109dcc8aca194e
write2cmdg <write2cmdg@gmail.com>
2023-04-04 03:14:33 -0400

    connected db

--- src/pages/BossesPage.jsx
+++ src/pages/BossesPage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 
@@ -6,6 +7,17 @@ import { Link } from 'react-router-dom'
 
 
 const BossesPage = () => {
+  const [ data, setData] = useState([])
+
+  useEffect(() => {
+  axios.get('http://localhost:4000/')
+  .then(res => setData(res.data))
+  .catch(err => console.log(err));
+  })
+
+
+
+  
 
   
   return (
@@ -13,8 +25,11 @@ const BossesPage = () => {
       <h2 className='text-xl md:text-2xl lg:text-3xl mx-auto w-2/3 p-6 text-3xl text-blue-900'>All Bosses</h2>
         <div className="w-screen min-h-[75%] flex bg-blue-100/50">
           <div className="w-1/2 flex flex-col items-center justify-center">
-    
-          
+          { 
+            data.map((d, i) => (
+              <Link to={"/"} >{d.name}</Link>
+            ))
+          }
           </div>    
         </div>
         <div className='z-50 text-xs md:text-lg lg:text-1xl mx-auto my-4 w-screen sm:w-2/3 flex flex-row justify-around cursor-pointer'>
