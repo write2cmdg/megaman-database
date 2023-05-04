3412627ac63db0fe7f4763c949c6be11fbfe3278
write2cmdg <write2cmdg@gmail.com>
2023-05-04 16:20:37 -0400

    create game page finished

--- src/pages/BossPage.jsx
+++ src/pages/BossPage.jsx
@@ -28,6 +28,8 @@ const BossPage = () => {
     }
   }
 
+  console.log(data)
+
   return (
 
     <div>
--- src/pages/GameCreatePage.jsx
+++ src/pages/GameCreatePage.jsx
@@ -1,17 +1,76 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link, Form } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom/dist'
+import Separator from '../components/Separator'
 
 const GameCreatePage = () => {
+  
+  const [values, setValues] = useState({
+    title: '',
+    year: '',
+    story: '',
+    bosses: ''
+  })
+  
+  const navigate = useNavigate()
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:4000/createGame', values)
+    .then(res => {navigate('/allGames')})
+    .catch(err => console.log(err));
+  
+  }
+
   return (
-    <>
     
-      <h2 className='text-xl md:text-2xl lg:text-3xl mx-auto w-2/3 p-6 text-3xl text-blue-900'>Create game</h2>
-        <div className="w-screen min-h-[75%] flex bg-blue-100/50">
-            {/* content */}
-        </div>
-        <div className='text-xs md:text-lg lg:text-1xl mx-auto my-4 w-screen sm:w-2/3 flex flex-row justify-around'>
-        </div> 
-    </>
+    <div>
+
+      <Separator />
+
+      <div className="bg-slate-800/90 flex flex-row justify-evenly">
+
+        <h2 className='
+        p-4 justify-center mx-auto
+        text-blue-200 text-xl w-screen md:text-3xl text-center font-press-start uppercase
+        '>new Game</h2>
+
+      </div>
+
+      <div className='text-blue-200 font-bold bg-slate-800/90'>
+
+        <Form onSubmit={handleSubmit} className="px-8 sm:w-3/4 xl:w-1/2 w-screen p-2 flex flex-col text-left mx-auto uppercase">
+
+          <div className="mb-2 flex flex-row justify-between">
+            <label className='w-1/3 font-press-start' htmlFor="title">Title: </label>
+            <input className='w-1/2 pl-3 bg-blue-200 text-blue-900 capitalize border-blue-900 border-4 p-1 rounded-lg' type="text" name="title" id="title" placeholder='Required' onChange={e => setValues({...values, title: e.target.value})}/>
+          </div>
+
+          <div className="mb-2 flex flex-row justify-between">
+            <label className='w-1/3 font-press-start' htmlFor="year">year: </label>
+            <input className='w-1/2 pl-3 bg-blue-200 text-blue-900 capitalize border-blue-900 border-4 p-1 rounded-lg' type="text" name="year" id="year" placeholder='Required'  onChange={e => setValues({...values, year: e.target.value})} />
+          </div>
+
+          <div className="mb-2 flex flex-row justify-between">
+            <label className='w-1/3 font-press-start' htmlFor="story">story: </label>
+            <input className='w-1/2 pl-3 bg-blue-200 text-blue-900 capitalize border-blue-900 border-4 p-1 rounded-lg' type="text" name="story" id="story" placeholder='Required'  onChange={e => setValues({...values, story: e.target.value})} />
+          </div>
+
+          <div className="mb-2 flex flex-row justify-between">
+            <label className='w-1/3 font-press-start' htmlFor="bosses">bosses: </label>
+            <input className='w-1/2 pl-3 bg-blue-200 text-blue-900 capitalize border-4 p-1  border-blue-900 rounded-lg' type="text" name="bosses" id="bosses" placeholder='Required'  onChange={e => setValues({...values, bosses: e.target.value})} />
+          </div>
+
+          <button className="uppercase mt-8 font-press-start text-lg hover:text-yellow-200 hover:bg-red-800 hover:border-red-900 text-blue-200 capitalize bg-blue-900 my-2 border-blue-900 border-4 p-1 rounded-lg">Submit</button>
+
+        </Form>
+
+      </div>
+
+      <Separator />
+
+    </div>
   )
 }
 
