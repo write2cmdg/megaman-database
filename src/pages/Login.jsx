20f6aa603d891b87ab12ee019decd2dedce0c32c
write2cmdg <write2cmdg@gmail.com>
2023-05-11 21:55:01 -0400

    set up login register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import { Form } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Form, Link } from 'react-router-dom'
 import Separator from '../components/Separator'
 
 const Login = () => {
+
+  const [ email, setEmail] = useState('')
+  const [ password, setPassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
     <div>
 
@@ -15,17 +24,23 @@ const Login = () => {
             '>Login</h2>
           </div>
           <div className='text-blue-200 font-bold bg-slate-800/90'>
-            <Form className="px-8 sm:w-3/4 xl:w-1/2 w-screen p-2 flex flex-col text-left mx-auto uppercase">
-              <div className="mb-2 flex flex-row justify-between">
-                <label className='w-1/3 font-press-start' htmlFor="email">email: </label>
-                <input className='w-1/2 pl-3 bg-blue-200 text-blue-900 capitalize border-blue-900 border-4 p-1 rounded-lg' type="text" name="email" id="email" placeholder='Required' onChange={e => setValues({...values, email: e.target.value})}/>
-              </div>
-              <div className="mb-2 flex flex-row justify-between">
-                <label className='w-1/3 font-press-start' htmlFor="pwd">password: </label>
-                <input className='w-1/2 pl-3 bg-blue-200 text-blue-900 capitalize border-blue-900 border-4 p-1 rounded-lg' type="text" name="pwd" id="pwd" placeholder='Required'  onChange={e => setValues({...values, pwd: e.target.value})} />
-              </div>
-              <button className="uppercase mt-8 font-press-start text-lg hover:text-yellow-200 hover:bg-red-800 hover:border-red-900 text-blue-200 capitalize bg-blue-900 my-2 border-blue-900 border-4 p-1 rounded-lg">Submit</button>
-            </Form>
+          <Form className="px-8 sm:w-3/4 xl:w-1/2 w-screen p-2 flex flex-col text-left mx-auto uppercase" onSubmit={handleSubmit}>
+
+          <div className="mb-2 flex flex-row justify-between">
+            <label className='w-1/3 font-press-start' htmlFor="email">email: </label>
+            <input className='w-1/2 pl-3 bg-blue-200 text-blue-900 capitalize border-blue-900 border-4 p-1 rounded-lg' type="email" name="email" id="email" placeholder='Required' onChange={(e) => setEmail(e.target.value)} value={email}/>
+          </div>
+
+          <div className="mb-2 flex flex-row justify-between">
+            <label className='w-1/3 font-press-start' htmlFor="password">password: </label>
+            <input className='w-1/2 pl-3 bg-blue-200 text-blue-900 capitalize border-blue-900 border-4 p-1 rounded-lg' type="password" name="password" id="password" placeholder='Required'  onChange={(e) => setPassword(e.target.value)} value={password} />
+          </div>
+
+          <button className="uppercase mt-8 font-press-start text-lg hover:text-yellow-200 hover:bg-red-800 hover:border-red-900 text-blue-200 capitalize bg-blue-900 my-2 border-blue-900 border-4 p-1 rounded-lg">sign up</button>
+
+
+          </Form>
+            <p>Need an Account? <Link to={'/register'} className='text-blue-600 hover:text-yellow-200'> Register Here</Link></p>
           </div>
           <Separator />
       </div>
